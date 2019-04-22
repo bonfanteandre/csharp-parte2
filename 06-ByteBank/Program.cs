67f89b046bbeb284da58828ec1439bd3c11b6053
André Bonfante <=>
2019-04-22 18:25:07 -0300

    Criando o repositório

---
+++ 06-ByteBank/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente cliente = new Cliente();
+            cliente.Nome = "André";
+
+            ContaCorrente conta = new ContaCorrente();
+            conta.Saldo = -10;
+            conta.Titular = cliente;
+
+            Console.WriteLine(conta.Saldo);
+            Console.WriteLine(conta.Titular.Nome);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 07-ByteBank/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaCorrente = new ContaCorrente(123, 456789);
+
+            Console.WriteLine(contaCorrente.Agencia);
+            Console.WriteLine(contaCorrente.Numero);
+
+            ContaCorrente contaCorrente2 = new ContaCorrente(321, 987654);
+
+            Console.WriteLine(contaCorrente2.Agencia);
+            Console.WriteLine(contaCorrente2.Numero);
+
+            Console.WriteLine(ContaCorrente.TotalContasCriadas);
+
+            Console.ReadKey();
+        }
+    }
+}
